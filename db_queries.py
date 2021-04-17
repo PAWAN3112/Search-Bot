748cbc93cefcf1439ce81d5375a82129c8710354
PAWAN KUMAR <>
2021-04-17 12:48:49 +0000

    "Output changes"

--- db_queries.py
+++ db_queries.py
@@ -14,7 +14,8 @@ def update_search_history(searched_words, user):
   db[len(db)+1] = json.dumps(result)
   # print(db['searched_words'])
   # return result['google_api_result']
-  return result['google_api_result']
+  output = "\n".join(list(result['google_api_result'].values()))
+  return output
 
 
 # To Retrieve db data containing specific keyword
@@ -24,7 +25,10 @@ def get_search_results(recent_searched_word):
 
   for key,val in db.items():
     if recent_searched_word in val:
-      searched_results.append(val)
+      print ("val", val, type(val))
+      val = json.loads(val)
+      searched_results.append(val["searched_words"])
+  output = "\n".join([str(a) for a in searched_results])
   # return db.getAll()
 
-  return searched_results
+  return output
